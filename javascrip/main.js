cd5b6c3fb4cddd25e61ec95660f07fc0c2a089a4
ANDRE  LUIS VIEIRA NUNES <34776@ETECPAOF.LAN>
2019-10-21 20:42:10 -0300

    Alterado 21/10/19 por giovana Não sabe

--- javascrip/main.js
+++ javascrip/main.js
@@ -1,32 +1,61 @@
-$(document). ready(fuction(){
+let urlAPI='https://gilsonpolito-api.herokuapp.com/alunos'
+$(document).ready(function(){
     $.ajax({
         type:'GET',
         contentType:'application/json',
         url:'https://gilsonpolito-api.herokuapp.com/alunos',
         dataType:'json',
-        sucess:function(data,textStatus,jqXHR){
+        success:function(data,textStatus,jqXHR){
          $.each(data, function(index,itemData){
           insereLinha(itemData.id,itemData.nome,itemData.site);
          });
         },
         error: function(jqXHR,textStatus,errorThrown){
          alert('Status: '+ textStatus + ' \nTipo:' +errorThrown +'\nMensagem:' + jqXHR.responseText);
+        
         }
      
-    })
-
-})
+    });
+});
 function insereLinha(id,nome,site){
     let linha='<tr>'+ 
-    '<td class=\'col-xs-2>'+
-    '<img src=\'imagens/editar.jpeg\' /></td><td> ' +
-     '<img src=\'imagens/editar.jpeg\' />'+
-     '</td><td class=\'col-xs-4'>' + 
-     nome + 
-     '</td>'
-     '<tdclass=\'col-xs-6'>+ 
-     site + 
-     '</td>'+
-     '</tr>';
-    $('#alunoTable').append(Linha);
+    '<td class=\"col-xs-2\">' +
+    '<a href="#" class="action_edit" value="' + id +
+    '"><img src="img/editar.jpeg" /></a>'+
+    '<a href="#" class="action_delete" value="' + id +
+    '"><img src="img/excluir.jpeg" /></a>'+
+    '</td>' +
+    '<td class=\"col-xs-4\">'+
+    nome +
+    '</td>'+
+    '<td class=\"col-xs-6\">'+
+    site +
+    '</td>'+
+    '</tr>';
+    $('#alunoTable').append(linha);
+}
+$('#add-to-list').on('click', (evento) =>{
+    evento.preventDefault();
+    
+    $.ajax({
+        type:'POST',
+        contentType:'application/json',
+        url:urlAPI,
+        dataType:'json',
+        data: formToJSON(),
+        success: function(data,textStatus,jqXHR){
+            insereLinha(data.id,data.nome,data.site)
+        },
+        error: function(jqXHR,textStatus,errorThrown){
+            alert('Status: '+ textStatus + ' \nTipo:' +errorThrown +'\nMensagem:' + jqXHR.responseText);
+        }
+    })
+})
+function formToJSON(){
+     return JSON.stringfy( {
+             "id": $('#idHidden').val(),
+             "nome": $('#nomeId').val(),
+             "site": $('#emailId').val(),
+            
+      });     
 }
